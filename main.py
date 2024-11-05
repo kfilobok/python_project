45a2a8abb1c1ee1b89c05ec78935c8278a33fd9b
kfilobok <94979665+kfilobok@users.noreply.github.com>
2024-11-05 10:57:37 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import parser
+import website
+
+def uploading():
+    # функция выгрузки данных на сайт
+    pass
+
+def main():
+    # функция поддерживающая постоянную работу приложения и обновление данных
+    pass
+
+
+if __name__ == "__main__":
+    main()
