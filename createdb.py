6bd77796424db35b8f258a4bdfff88e078526e46
Polina Korovina <korovinapolina418@gmail.com>
2024-12-21 18:25:39 +0300

    1

--- createdb.py
+++ createdb.py
@@ -15,5 +15,24 @@ CREATE TABLE IF NOT EXISTS products (
 )
 ''')
 
+DATABASE = 'products.db'
+
+create_users_table_query = '''
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL
+);
+'''
+
+with sqlite3.connect(DATABASE) as conn:
+    cursor = conn.cursor()
+    cursor.execute(create_users_table_query)
+    conn.commit()
+
+
+
+
 conn.commit()
 conn.close()
